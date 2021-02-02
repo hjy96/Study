4109bf5f6eb830eb0abdf22549d888f5e05ae27d
준영 한 <althdi96@naver.com>
2021-02-02 10:36:30 +0900

    Add 'Divide'

--- 7. Inheritance2.py
+++ 7. Inheritance2.py
@@ -7,16 +7,21 @@ class Cal ( object ):
 		return self.v1 + self.v2
 	def subtract ( self ):
 		return self.v1 - self.v2
-	def multiply ( self ):
-		return self.v1 * self.v2
 	def setV1 ( self, v ):
 		if isinstance( v, int ):
 			self.v1 = v
 	def getV1 ( self ):
 		return self.v1
 class CalMultiply ( Cal ):
-	def multuply ( slef ):
-		return self.v1 * selfv2
+	def multiply ( self ):
+		return self.v1 * self.v2
+class CalDivide ( CalMultiply ) :
+	def divide ( self ) :
+		return self.v1 / self.v2
 c1 = CalMultiply( 10,10 )
 print( c1.add() )
 print( c1.multiply() )
+c2 = CalDivide( 20,10 )
+print( c2, c2.add() )
+print( c2, c2.multiply() )
+print( c2, c2.divide() )
