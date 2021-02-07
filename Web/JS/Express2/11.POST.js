89511134682078953403b82c8d62522f6b76e607
hjy <althdi96@gmail.com>
2021-02-07 17:23:32 +0900

    home

--- Web/JS/Express2/11.POST.js
+++ Web/JS/Express2/11.POST.js
@@ -1,26 +1,32 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');        //addParser module
 app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', '../views');
 const port = 3000;
 app.use(express.static('../public'));
+app.use(bodyParser.urlencoded({ extended: false }))     //for use post
 
 app.get('/form', function (req, res) {
     res.render('form');
 });             //add, view attribute
 
-app.get('/form_receiver', function(req, res){
+app.get('/form_receiver', function (req, res) {
+    res.send('Hello,GET');
     var title = req.query.title;
     var description = req.query.description;
-    res.send(title+','+description);
+    res.send(title + ',' + description);
 })      //route
 
-app.post('form_receiver', function(req, res){
-    res.send('Hello, POST');
+app.post('/form_receiver', function (req, res) {
+    //res.send('Hello, POST');
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send(title + ',' + description);
 })      //POST
 
-app.get('/topic/:id', function (req, res) {       
+app.get('/topic/:id', function (req, res) {
 
     var topics = [
         'Javascript is...',
@@ -29,15 +35,15 @@ app.get('/topic/:id', function (req, res) {
     ];
 
     var output = `
-<a href="/topic?id=0">JavaScript</a><br>
-<a href="/topic?id=1">Nodejs</a><br>
-<a href="/topic?id=2">Express</a><br>
+<a href="/topic/0">JavaScript</a><br>
+<a href="/topic/1">Nodejs</a><br>
+<a href="/topic/2">Express</a><br>
 ${topics[req.params.id]}                   
 `
-    res.send(output);               
+    res.send(output);
 });
 app.get('/topic/:id/:mode', function (req, res) {
-    res.send(req.params.id + ',' + req.params.mode)         
+    res.send(req.params.id + ',' + req.params.mode)
 })
 app.get('/template', function (req, res) {
     res.render('temp', { time: Date(), _title: 'Jade' });
