cf7c2e0abe5c7a782606ee69a018e05d476c0ef6
hjy <althdi96@gmail.com>
2021-02-26 17:23:29 +0900

    Save the board

--- RMBoard/routes/users.js
+++ RMBoard/routes/users.js
@@ -16,17 +16,22 @@ router.get('/', function (req, res) {
 
 // New
 router.get('/new', function (req, res) {
-	res.render('users/new');
+	var user = req.flash('user')[0] || {};
+	var errors = req.flash('errors')[0] || {};
+	res.render('users/new', { user: user, errors: errors });
 })
 
 // Create
 router.post('/', function (req, res) {
 	User.create(req.body, function (err, user) {
-		if (err) return res.json(err);
+		if (err) {
+			req.flash('user', req.body);
+			req.flash('errors', parseError(err));
+			return res.redirect('/users/new');
+		}
 		res.redirect('/users');
 	});
 });
-
 // Show
 router.get('/:username', function (req, res) {
 	User.findOne({ username: req.params.username }, function (err, user) {
@@ -37,31 +42,42 @@ router.get('/:username', function (req, res) {
 
 // Edit
 router.get('/:username/edit', function (req, res) {
-	User.findOne({ username: req.params.username }, function (err, user) {
-		if (err) return res.json(err);
-		res.render('users/edit', { user: user });
-	});
+	var user = req.flash('user')[0];
+	var errors = req.flash('errors')[0] || {};
+	if (!user) {
+		User.findOne({ username: req.params.username }, function (err, user) {
+			if (err) return res.json(err);
+			res.render('users/edit', { username: req.params.username, user: user, errors: errors });
+		});
+	}
+	else {
+		res.render('users/edit', { username: req.params.username, user: user, errors: errors });
+	}
 });
-
 // Update
 router.put('/:username', function (req, res, next) {
 	User.findOne({ username: req.params.username })
 		.select('password')
 		.exec(function (err, user) {
-		if (err) return res.json(err);
-
-		// update user object
-		user.originalPassword = user.password;
-		user.password = req.body.newPassword ? req.body.newPassword : user.password;
-		for (var p in req.body) {
-			user[p] = req.body[p];
-		}
-		// save update user
-		user.save(function (err, user) {
 			if (err) return res.json(err);
-			res.redirect('/users/' + user.username);
+
+			// update user object
+			user.originalPassword = user.password;
+			user.password = req.body.newPassword ? req.body.newPassword : user.password;
+			for (var p in req.body) {
+				user[p] = req.body[p];
+			}
+
+			// save updated user
+			user.save(function (err, user) {
+				if (err) {
+					req.flash('user', req.body);
+					req.flash('errors', parseError(err));
+					return res.redirect('/users/' + req.params.username + '/edit');
+				}
+				res.redirect('/users/' + user.username);
+			});
 		});
-	});
 });
 
 
@@ -74,3 +90,21 @@ router.delete('/:username', function (req, res) {
 });
 
 module.exports = router;
+
+// functions
+function parseError(errors) {
+	var parsed = {};
+	if (errors.name == 'ValidationError') {
+		for (var name in errors.errors) {
+			var validationError = errors.errors[name];
+			parsed[name] = { message: validationError.message };
+		}
+	}
+	else if (errors.code == '11000' && errors.errmsg.indexOf('username') > 0) {
+		parsed.username = { message: 'This username already exists!' };
+	}
+	else {
+		parsed.unhandled = JSON.stringify(errors);
+	}
+	return parsed;
+}
